ffc5fd4c591b34b501a1e49ed3790553c45d0fc5
grace-shearrer <grace.shearrer@gmail.com>
2015-10-06 15:21:17 -0500

    trying to get the last part of the command k to work

--- make_roi.py
+++ make_roi.py
@@ -9,7 +9,8 @@ from subprocess import check_output
 
 def make_roi (region, cope_num):
   for file in glob.glob('/corral-repl/utexas/poldracklab/data/sugar_brain/sb_00*/model/Lev2/lev2_taste_fnirt.gfeat/cope'+cope_num+'.feat/stats/cope1.nii.gz'): 
-    x=subprocess.call(['fslstats',file,'-m', '-k /corral-repl/utexas/poldracklab/data/sugar_brain/ROIs/',region,'.nii.gz']) 
+    y= region+'nii.gz'
+    x=subprocess.call(['fslstats',file,'-m', '-k /corral-repl/utexas/poldracklab/data/sugar_brain/ROIs/',y]) 
     print x
 
 def main():
