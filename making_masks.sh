df4b013cb1a08bd84b9944652401f044e7c685e6
grace-shearrer <grace.shearrer@gmail.com>
2015-10-06 16:54:32 -0500

    i think it needed the .nii.gz

--- making_masks.sh
+++ making_masks.sh
@@ -1,5 +1,7 @@
-for x in /corral-repl/utexas/poldracklab/data/sugar_brain/sb_001*/model/feat/taste_run*/reg/highres2standard_warp.nii.gz;
+for x in /corral-repl/utexas/poldracklab/data/sugar_brain/sb_0010*/model/feat/taste_run*/reg/highres2standard_warp.nii.gz;
 do
-  invwarp -w $x -o standard2highres_warp -r highres | applywarp -i /corral-repl/utexas/poldracklab/data/sugar_brain/ROIs/RCaudate_mask.nii.gz -r example_func -o RCaudateMaskFunc -w standard2highres_warp --postmat=highres2example_func.mat | fslmaths RCaudateMaskFunc.nii.gz -thr 0.5 -bin RCaudateMaskFuncBin.nii.gz
+  echo "$x"
+  #invwarp -w $x -o standard2highres_warp.nii.gz -r highres | applywarp -i /corral-repl/utexas/poldracklab/data/sugar_brain/ROIs/RCaudate_mask.nii.gz -r example_func -o RCaudateMaskFunc -w standard2highres_warp --postmat=highres2example_func.mat | fslmaths RCaudateMaskFunc.nii.gz -thr 0.5 -bin RCaudateMaskFuncBin.nii.gz
+  echo "done"
 done
  
